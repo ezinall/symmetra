ffcc94da01d3562acd14b06f1d38644cd9f52390
VasilevskiyRA <vasilevskiyra@byterg.ru>
2020-10-09 19:49:22 +0300

    add redis listener

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from collections import defaultdict
 
 import uvloop
 from aiohttp import web, WSCloseCode
-
+import aioredis
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
-async def chanel_list(request):
+async def channel_list(request):
     return web.json_response(data=list(request.app['websockets'].keys()))
 
 
@@ -35,6 +35,31 @@ async def websocket_handler(request):
     return ws
 
 
+async def listen_to_redis(app):
+    try:
+        ch, *_ = await app['redis'].psubscribe('ws.*')
+        async for channel, msg in ch.iter(encoding='utf-8'):
+            *_, socket_channel = channel.decode().split('.')
+            for i in set(app['websockets'][socket_channel]):
+                await i.send_str(msg)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        if not app['redis'].closed:
+            await app['redis'].punsubscribe(ch.name)
+
+
+async def start_background_tasks(app):
+    app['redis_listener'] = asyncio.create_task(listen_to_redis(app))
+
+
+async def cleanup_background_tasks(app):
+    app['redis_listener'].cancel()
+    await app['redis_listener']
+    app['redis'].close()
+    await app['redis'].wait_closed()
+
+
 async def on_shutdown(app):
     for ws in set(ws for ref in app['websockets'].values() for ws in ref):
         await ws.close(code=WSCloseCode.GOING_AWAY,
@@ -43,14 +68,20 @@ async def on_shutdown(app):
 
 async def create_app(*args, **kwargs):
     app = web.Application()
+    redis = await aioredis.create_redis_pool('redis://localhost')
 
-    app.update(websockets=defaultdict(weakref.WeakSet))
+    app.update(
+        redis=redis,
+        websockets=defaultdict(weakref.WeakSet)
+    )
 
     app.add_routes([
-        web.get('/list', chanel_list),
+        web.get('/list', channel_list),
         web.get('/ws/{channel}', websocket_handler),
     ])
 
+    app.on_startup.append(start_background_tasks)
+    app.on_cleanup.append(cleanup_background_tasks)
     app.on_shutdown.append(on_shutdown)
 
     return app
